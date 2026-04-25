5ac04204820b2f63223208b3c05e5462b70346ed
shota <shota.shimabuku@systemsengineeya.jp>
2026-04-25 11:06:19 +0900

    初期コミット

---
+++ book-review-api/src/main/java/jp/systemsengineeya/bookreview/api/BookReviewApiApplication.java
@@ -0,0 +1,13 @@
+package jp.systemsengineeya.bookreview.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewApiApplication.class, args);
+	}
+
+}
---
+++ book-review-api/src/test/java/jp/systemsengineeya/bookreview/api/BookReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.systemsengineeya.bookreview.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
