722ceaaf04f8c335e4c97d7b429b7217df4845b6
systemengineeya <188732536+systemengineeya@users.noreply.github.com>
2026-05-05 00:49:15 +0900

    ReviewのDTOをrequestとresponseに分ける

--- book-review-api/src/main/java/jp/systemengineeya/bookreview/api/dto/response/ReviewResponse.java
+++ book-review-api/src/main/java/jp/systemengineeya/bookreview/api/dto/response/ReviewResponse.java
@@ -1,4 +1,4 @@
-package jp.systemengineeya.bookreview.api.dto;
+package jp.systemengineeya.bookreview.api.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ReviewDto {
+public class ReviewResponse {
     private Long id;
     private Long bookId;
     private String content;
