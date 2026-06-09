834fc155d9bcb2dc4a98536cfea23c0f8692a26e
systemengineeya <188732536+systemengineeya@users.noreply.github.com>
2026-06-09 16:03:50 +0900

    AWS上で動くよう対応 (#9)
    * S3Configでリージョン指定をする
    
    * LocalStack初期化スクリプトが実行できない問題を修正
    
    - シェルスクリプトの改行コードを LF に統一
    
    * profile未指定の場合にflywayでlocalのDMLが流れないよう対応
    
    * リファクタリング
    
    - 不要なimportの削除
    
    * gitattributesの移動
    
    全プロジェクトに効くよう対応

--- book-review-api/src/main/java/jp/systemengineeya/bookreview/api/config/S3Config.java
+++ book-review-api/src/main/java/jp/systemengineeya/bookreview/api/config/S3Config.java
@@ -1,24 +1,31 @@
 package jp.systemengineeya.bookreview.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Profile("prod")
-public class ProdS3Config {
+@Profile("!local")
+public class S3Config {
+    @Value("${cloud.aws.region}")
+    private String region;
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .region(Region.of(region))
                 .build();
     }
 
     @Bean
     public S3Presigner s3Presigner() {
-        return S3Presigner.builder().build();
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .build();
     }
 }
